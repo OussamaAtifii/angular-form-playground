c567e99a8b5051b34c77a16e5add7375a4b986f5
OussamaAtifii <oussamaati03@gmail.com>
2026-04-05 14:12:48 +0200

    feat: improve base form-field abstract class

--- src/app/core/models/form-field.ts
+++ src/app/core/models/form-field.ts
@@ -1,16 +1,14 @@
 import { FieldType } from '@core/types/field-type';
 
 export abstract class FormField {
-  id: string;
-  label: string;
-  placeholder: string;
-  required: boolean;
+  public id: string = crypto.randomUUID();
 
-  constructor(label: string, placeholder: string) {
-    this.id = crypto.randomUUID();
+  constructor(
+    public label: string,
+    public placeholder: string,
+  ) {
     this.label = label;
     this.placeholder = placeholder;
-    this.required = false;
   }
 
   abstract getType(): FieldType;
