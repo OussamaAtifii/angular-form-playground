985be886aed254928ec46d286a797e609c9c9de8
OussamaAtifii <oussamaati03@gmail.com>
2026-04-10 23:06:28 +0200

    feat: show fields label instead of id in results dialog

--- src/app/features/form-builder/components/form-results-dialog/form-results-dialog.ts
+++ src/app/features/form-builder/components/form-results-dialog/form-results-dialog.ts
@@ -1,5 +1,5 @@
-import { JsonPipe, KeyValuePipe } from '@angular/common';
-import { Component, inject, input, viewChild } from '@angular/core';
+import { JsonPipe, KeyValuePipe, NgClass } from '@angular/common';
+import { Component, computed, inject, input, viewChild } from '@angular/core';
 import { FormData } from '@form-builder/types/form-data';
 import { Dialog } from '@shared/components/dialog/dialog';
 import { ArrowLeft } from '@icons/arrow-left/arrow-left';
@@ -7,21 +7,17 @@ import { FormBuilderService } from '@form-builder/services/form-builder-service'
 
 @Component({
   selector: 'app-form-results-dialog',
-  imports: [Dialog, KeyValuePipe, ArrowLeft, ArrowLeft],
+  imports: [Dialog, KeyValuePipe, ArrowLeft, ArrowLeft, NgClass],
   templateUrl: './form-results-dialog.html',
 })
 export class FormResultsDialog {
   dialog = viewChild<Dialog>(Dialog);
-
   formBuilderService = inject(FormBuilderService);
-
-  fields = this.formBuilderService.fields;
 
   formData = input.required<FormData>();
 
-  getFieldLabel(fieldId: string) {
-    return this.fields().find((field) => field.id === fieldId)?.label;
-  }
+  fields = this.formBuilderService.fields;
+  fieldsLabelMap = computed(() => new Map(this.fields().map((field) => [field.id, field.label])));
 
   open() {
     this.dialog()?.open();
